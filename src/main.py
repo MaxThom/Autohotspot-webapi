f4608e5004040bc893d5054262e87a454cb24510
MaxThom <maxthomassin@hotmail.com>
2021-09-07 10:52:29 +0100

    added documentation

--- src/main.py
+++ src/main.py
@@ -6,17 +6,9 @@ from flask import Flask, request, jsonify
 from hotspot.hotspot import hotspot_bp
 import animation as anim
 
-
-
-#app.logger.debug('This is a DEBUG message')
-#app.logger.info('This is an INFO message')
-#app.logger.warning('This is a WARNING message')
-#app.logger.error('This is an ERROR message')
-
-
 class Main:
     def __init__(self):        
-        print("Born wold !")
+        print("Hello world !")
         self.app = Flask(__name__)
         self.app.register_blueprint(hotspot_bp, url_prefix='/')
 
@@ -28,8 +20,6 @@ class Main:
     def main(self):
         self.th_animation.start()
         self.app.run(host='0.0.0.0', port=80)
-
-        
 
 if __name__ == '__main__':
     main = Main()
